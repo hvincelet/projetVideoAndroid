19aa7d9d522f8f02e9e6822e521e95ccc4a3053f
Hugo Vincelet <hugo.vincelet@gmail.com>
2018-11-15 09:30:42 +0100

    Integrated VideoView

--- app/src/main/java/fr/enssat/deroovincelet/MainActivity.java
+++ app/src/main/java/fr/enssat/deroovincelet/MainActivity.java
@@ -1,13 +1,77 @@
 package fr.enssat.deroovincelet;
 
+import android.app.ProgressDialog;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private VideoView myVideoView;
+    /*private VideoWebViewModel mViewModel;
+    private MediaPlayer mMediaPlayer;*/
+    private int position = 0;
+    private ProgressDialog progressDialog;
+    private MediaController mediaControls;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (mediaControls == null) {
+            mediaControls = new MediaController(MainActivity.this);
+        }
+
+        myVideoView = (VideoView) findViewById(R.id.video_view);
+
+        progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setTitle("JavaCodeGeeks Android Video View Example");
+        progressDialog.setMessage("Loading...");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+
+        try {
+            myVideoView.setMediaController(mediaControls);
+
+            myVideoView.setVideoURI(Uri.parse("https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"));
+        } catch (Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+
+        myVideoView.requestFocus();
+        myVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                progressDialog.dismiss();
+                myVideoView.seekTo(position);
+                if (position == 0) {
+                    myVideoView.start();
+                } else {
+                    myVideoView.pause();
+                }
+            }
+        });
+
+    }
+
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("Position", myVideoView.getCurrentPosition());
+        myVideoView.pause();
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        position = savedInstanceState.getInt("Position");
+        myVideoView.seekTo(position);
     }
 }
